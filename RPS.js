e10c44bfc07cc56c2b79b503274dc1c4a44b0c13
Abdul-KMC <mfnadwi0522@gmail.com>
2023-10-31 15:57:07 -0300

    fix: updated code styling

--- RPS.js
+++ RPS.js
@@ -1,9 +1,11 @@
+// ** get the computers choice **
 function getComputerChoice() {
     let rpsChoices = ['Rock', 'Paper', 'Scissors']
     let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
     return computerChoice
 }
 
+// ** back button and it's functionality **
 const backButton = document.getElementById('back');
 backButton.addEventListener('click', () => {
     window.location.href = 'playmingle.html';
@@ -17,43 +19,27 @@ select.addEventListener("change", function() {
     }
 });
 
-// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
-// human wins - getResult('Rock', 'Scissors') 👉 1
-// human loses - getResult('Scissors', 'Rock') 👉 -1
-// human draws - getResult('Rock', 'Rock') 👉 0
+// ** get the scores for current round **
 function getResult(playerChoice, computerChoice) {
-    // return the result of score based on if you won, drew, or lost
 
     let score;
-
-    // All situations where human draws, set `score` to 0
     if (playerChoice === computerChoice) {
         score = 0
-
-
     } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
         score = 1
-
     } else if (playerChoice === "Paper" && computerChoice === "Rock") {
         score = 1
 
     } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
         score = 1
-
-        // Otherwise human loses (aka set score to -1)
     } else {
         score = -1
     }
-
-    // return score
     return score
 }
 
 // ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
 function showResult(score, playerChoice, computerChoice) {
-    // Hint: on a score of -1
-    // You should do result.innerText = 'You Lose!'
-    // Don't forget to grab the div with the 'result' id!
 
     let result = document.getElementById('result')
     switch (score) {
